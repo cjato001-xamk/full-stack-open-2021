8ae74d8af3c9f92f7e92b45208027caa1febc0c1
Janne Toikander <janne.toikander@gmail.com>
2021-08-27 23:39:38 +0300

    Match blog tests with tasks 5.13 and 5.14

--- part5/blog-list-frontend/src/components/Blog.test.tsx
+++ part5/blog-list-frontend/src/components/Blog.test.tsx
@@ -1,31 +1,55 @@
-import { render, screen } from '@testing-library/react'
+import { queryByText, render, screen, fireEvent } from '@testing-library/react'
+import { IBlog } from '../interfaces/IBlog'
 import { Blog } from './Blog'
 
 describe('Blog component', () => {
-  it('renders a blog', () => {
-    const mockBlog = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let container: any
+  let mockBlog: IBlog
+
+  beforeEach(() => {
+    mockBlog = {
       id: 'test-id',
       title: 'test-title',
       author: 'test-author',
-      url: '',
-      likes: 0,
+      url: 'test-url',
+      likes: 42,
       user: {
         id: '',
         username: '',
         name: '',
       },
     }
-
-    render(
+    ;({ container } = render(
       <Blog
         blog={mockBlog}
         setNotification={jest.fn()}
         refreshBlogs={jest.fn()}
       />
-    )
+    ))
+  })
+
+  it('renders a blog without details', () => {
+    expect(
+      screen.getByText(`${mockBlog.title} ${mockBlog.author}`)
+    ).toBeInTheDocument()
+    expect(queryByText(container, mockBlog.url)).toBeFalsy()
+    expect(queryByText(container, mockBlog.likes)).toBeFalsy()
+  })
+
+  it('renders a blog with details after button clicked', () => {
+    expect(
+      screen.getByText(`${mockBlog.title} ${mockBlog.author}`)
+    ).toBeInTheDocument()
+    expect(queryByText(container, mockBlog.url)).toBeFalsy()
+    expect(queryByText(container, mockBlog.likes)).toBeFalsy()
+
+    fireEvent.click(screen.getByText('Show details'))
 
     expect(
       screen.getByText(`${mockBlog.title} ${mockBlog.author}`)
     ).toBeInTheDocument()
+    expect(screen.getByText(mockBlog.url)).toBeInTheDocument()
+    expect(screen.getByText(`Likes: ${mockBlog.likes}`)).toBeInTheDocument()
   })
 })
