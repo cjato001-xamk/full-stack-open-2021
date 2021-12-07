6b74ebac42b7c1bf83f8ceac1ac8e893cdce8b36
Janne Toikander <janne.toikander@gmail.com>
2021-12-07 23:31:38 +0200

    Task 9.5, http endpoint for the body mass index

--- part9/ts-starters/index.ts
+++ part9/ts-starters/index.ts
@@ -1,12 +1,33 @@
 import express from 'express';
 
+import { calculateBmi } from './bmiCalculator';
+
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  let { height, weight }: any = req.query;
+
+  if (!height || !weight || isNaN(Number(height)) || isNaN(Number(weight))) {
+    return res.json({
+      error: 'Invalid parameters.',
+    });
+  }
+
+  const bmi = calculateBmi(height, weight);
+
+  return res.json({
+    weight,
+    height,
+    bmi,
+  });
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
