f174af1860c0a54eefc56e9a379c089a9adc3ad0
Janne Toikander <janne.toikander@gmail.com>
2021-12-20 22:41:41 +0200

    Task 9.26 and 9.27, allow to add any entry type and added client side validation

--- part9/patientor-frontend/src/AddEntryModal/AddEntryForm.tsx
+++ part9/patientor-frontend/src/AddEntryModal/AddEntryForm.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 import { useStateValue } from '../state';
 import { NewEntry, EntryType } from '../types';
+import { assertNever } from '../utils/assertNever';
 
 import {
   DiagnosisSelection,
@@ -14,7 +15,7 @@ import {
   TextField,
 } from '../components/FormField';
 
-const EntrySchema = Yup.object().shape({
+const baseSchema = Yup.object().shape({
   description: Yup.string().required('Field is required.'),
   date: Yup.string()
     .trim()
@@ -46,33 +47,124 @@ const entryTypeOptions: EntryTypeOption[] = [
 
 const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
   const [{ diagnoses }] = useStateValue();
+  const [selectedEntryType, setSelectedEntryType] = useState<EntryType>(
+    EntryType.HealthCheck
+  );
+  const [validationSchema, setValidationSchema] = useState(baseSchema);
+
+  let initialValues = {
+    type: selectedEntryType,
+    description: '',
+    date: '',
+    specialist: '',
+    diagnosisCodes: [],
+  } as unknown as EntryFormValues;
+
+  useEffect(() => {
+    switch (selectedEntryType) {
+      case EntryType.HealthCheck:
+        initialValues = {
+          ...initialValues,
+          type: EntryType.HealthCheck,
+          healthCheckRating: 1,
+        };
+
+        setValidationSchema(
+          baseSchema.concat(
+            Yup.object().shape({
+              healthCheckRating: Yup.number()
+                .min(0)
+                .max(3)
+                .required('Field is required.'),
+            })
+          )
+        );
+        break;
+
+      case EntryType.OccupationalHealthCare:
+        initialValues = {
+          ...initialValues,
+          type: EntryType.OccupationalHealthCare,
+          employerName: '',
+          sickLeave: {
+            startDate: '',
+            endDate: '',
+          },
+        };
+
+        setValidationSchema(
+          baseSchema.concat(
+            Yup.object().shape({
+              employerName: Yup.string().required('Field is required.'),
+              sickLeave: Yup.object().shape({
+                startDate: Yup.string()
+                  .trim()
+                  // Regexp grabbed from https://stackoverflow.com/a/22061799 (and modified)
+                  // Does not actually validate it's a date, so might still fail on backend
+                  .matches(
+                    /^(20)\d{2}-((0[1-9])|(1[0-2]))-(0[1-9]|[1-2][0-9]|3[0-1])$/,
+                    'Invalid format.'
+                  ),
+                endDate: Yup.string()
+                  .trim()
+                  // Regexp grabbed from https://stackoverflow.com/a/22061799 (and modified)
+                  // Does not actually validate it's a date, so might still fail on backend
+                  .matches(
+                    /^(20)\d{2}-((0[1-9])|(1[0-2]))-(0[1-9]|[1-2][0-9]|3[0-1])$/,
+                    'Invalid format.'
+                  ),
+              }),
+            })
+          )
+        );
+        break;
+
+      case EntryType.Hospital:
+        initialValues = {
+          ...initialValues,
+          type: EntryType.Hospital,
+          discharge: {
+            date: '',
+            criteria: '',
+          },
+        };
+
+        setValidationSchema(
+          baseSchema.concat(
+            Yup.object().shape({
+              discharge: Yup.object().shape({
+                date: Yup.string()
+                  .trim()
+                  // Regexp grabbed from https://stackoverflow.com/a/22061799 (and modified)
+                  // Does not actually validate it's a date, so might still fail on backend
+                  .matches(
+                    /^(20)\d{2}-((0[1-9])|(1[0-2]))-(0[1-9]|[1-2][0-9]|3[0-1])$/,
+                    'Invalid format.'
+                  )
+                  .required('Field is required.'),
+                criteria: Yup.string().required('Field is required.'),
+              }),
+            })
+          )
+        );
+        break;
+
+      default:
+        assertNever(selectedEntryType);
+    }
+  }, [selectedEntryType]);
 
   return (
     <Formik
-      initialValues={{
-        type: EntryType.HealthCheck,
-        description: '',
-        date: '',
-        specialist: '',
-        diagnosisCodes: [],
-        healthCheckRating: 0,
-      }}
+      initialValues={initialValues}
       onSubmit={onSubmit}
-      validationSchema={EntrySchema}
+      validationSchema={validationSchema}
     >
-      {({
-        isValid,
-        dirty,
-        setFieldValue,
-        setFieldTouched,
-        values,
-        touched,
-        errors,
-      }) => {
+      {({ isValid, dirty, setFieldValue, setFieldTouched, values }) => {
         useEffect(() => {
-          console.log(values, touched, errors);
-        }, [values, touched, errors]);
-        // TODO: On values.type change, should undefine non-related fields
+          setSelectedEntryType(values.type);
+        }, [values.type]);
+
         return (
           <Form className='form ui'>
             <SelectField<EntryTypeOption>
@@ -108,7 +200,7 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
               diagnoses={Object.values(diagnoses)}
             />
 
-            {values.type === EntryType.HealthCheck && (
+            {selectedEntryType === EntryType.HealthCheck && (
               <Field
                 label='Health Check Rating'
                 name='healthCheckRating'
@@ -118,7 +210,7 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
               />
             )}
 
-            {values.type === EntryType.OccupationalHealthCare && (
+            {selectedEntryType === EntryType.OccupationalHealthCare && (
               <>
                 <Field
                   label='Employer'
@@ -126,24 +218,22 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
                   name='employerName'
                   component={TextField}
                 />
-                {/** // TODO: SickLeave start (optional) */}
                 <Field
                   label='Sick leave start date'
                   placeholder='YYYY-MM-DD'
                   name='sickLeave.startDate'
                   component={TextField}
                 />
-                {/** // TODO: SickLeave end (mandatory if start is set) */}
                 <Field
                   label='Sick leave end date'
                   placeholder='YYYY-MM-DD'
-                  name='sickLeave.startDate'
+                  name='sickLeave.endDate'
                   component={TextField}
                 />
               </>
             )}
 
-            {values.type === EntryType.Hospital && (
+            {selectedEntryType === EntryType.Hospital && (
               <>
                 <Field
                   label='Discharge date'
